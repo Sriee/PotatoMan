b112dd4aec94e606aac65460e7c5192e20b3f7ce
Sriee <psg.sriee@gmail.com>
2016-03-13 22:57:26 -0500

    Adding Game Files

---
+++ Game/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,203 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+
+
+public class PlayerControl : MonoBehaviour
+{
+    /*******************************************************************************************
+    * Speech Recognition Variables
+    ********************************************************************************************/
+    Thread controlsInputThread;
+    UdpClient gameClient;
+    public int port = 26000;
+    string strReceiveUDP = "";
+    //string localIP = "";
+    string hostname;
+
+    /******************************************************************************************/
+
+    [HideInInspector]
+	public bool facingRight = true;			// For determining which way the player is currently facing.
+	[HideInInspector]
+	public bool jump = false;				// Condition for whether the player should jump.
+
+
+	public float moveForce = 365f;			// Amount of force added to move the player left and right.
+	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
+	public AudioClip[] jumpClips;			// Array of clips for when the player jumps.
+	public float jumpForce = 1000f;			// Amount of force added when the player jumps.
+	public AudioClip[] taunts;				// Array of clips for when the player taunts.
+	public float tauntProbability = 50f;	// Chance of a taunt happening.
+	public float tauntDelay = 1f;			// Delay for when the taunt should happen.
+
+
+	private int tauntIndex;					// The index of the taunts array indicating the most recent taunt.
+	private Transform groundCheck;			// A position marking where to check if the player is grounded.
+	private bool grounded = false;			// Whether or not the player is grounded.
+	private Animator anim;					// Reference to the player's animator component.
+
+
+    void Start()
+    {
+        Application.OpenURL((Application.dataPath) + @"\Plugin\Test.exe");
+        Application.runInBackground = true;
+
+        controlsInputThread = new Thread(new ThreadStart(ReceiveData));
+        controlsInputThread.IsBackground = true;
+        controlsInputThread.Start();
+
+       /* hostname = Dns.GetHostName();
+        IPAddress[] ips = Dns.GetHostAddresses(hostname);
+        if (ips.Length > 0)
+            localIP = ips[0].ToString();*/
+    }//End of Start
+
+    private void ReceiveData()
+    {
+        gameClient = new UdpClient(port);
+        while (true)
+        {
+            try
+            {
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, port);
+                byte[] data = gameClient.Receive(ref anyIP);
+                strReceiveUDP = Encoding.UTF8.GetString(data);
+                Console.WriteLine(strReceiveUDP);
+            }
+            catch (Exception e)
+            {
+                print(e.Message + "@ReceiveData");
+            }
+        }
+    }//End of ReceiveData
+
+
+    void Awake()
+	{
+		// Setting up references.
+		groundCheck = transform.Find("groundCheck");
+		anim = GetComponent<Animator>();
+	}
+
+
+	void Update()
+	{
+		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        // If the jump button is pressed and the player is grounded then the player should jump.
+        /*if(Input.GetButtonDown("Jump") && grounded)
+			jump = true;
+        */ //Revert Back
+        if (strReceiveUDP.Equals("Jump",StringComparison.OrdinalIgnoreCase) && grounded)
+            jump = true;
+
+    }
+
+
+	void FixedUpdate ()
+	{
+		// Cache the horizontal input.
+		float h = Input.GetAxis("Horizontal");
+
+		// The Speed animator parameter is set to the absolute value of the horizontal input.
+		anim.SetFloat("Speed", Mathf.Abs(h));
+
+		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+		if(h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
+			// ... add a force to the player.
+			GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
+
+		// If the player's horizontal velocity is greater than the maxSpeed...
+		if(Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
+			// ... set the player's velocity to the maxSpeed in the x axis.
+			GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		// If the input is moving the player right and the player is facing left...
+		if(h > 0 && !facingRight)
+			// ... flip the player.
+			Flip();
+		// Otherwise if the input is moving the player left and the player is facing right...
+		else if(h < 0 && facingRight)
+			// ... flip the player.
+			Flip();
+
+		// If the player should jump...
+		if(jump)
+		{
+			// Set the Jump animator trigger parameter.
+			anim.SetTrigger("Jump");
+
+			// Play a random jump audio clip.
+			int i = UnityEngine.Random.Range(0, jumpClips.Length);
+			AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
+
+			// Add a vertical force to the player.
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+
+			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
+			jump = false;
+		}
+	}
+	
+	
+	void Flip ()
+	{
+		// Switch the way the player is labelled as facing.
+		facingRight = !facingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+
+	public IEnumerator Taunt()
+	{
+		// Check the random chance of taunting.
+		float tauntChance = UnityEngine.Random.Range(0f, 100f);
+		if(tauntChance > tauntProbability)
+		{
+			// Wait for tauntDelay number of seconds.
+			yield return new WaitForSeconds(tauntDelay);
+
+			// If there is no clip currently playing.
+			if(!GetComponent<AudioSource>().isPlaying)
+			{
+				// Choose a random, but different taunt.
+				tauntIndex = TauntRandom();
+
+				// Play the new taunt.
+				GetComponent<AudioSource>().clip = taunts[tauntIndex];
+				GetComponent<AudioSource>().Play();
+			}
+		}
+	}
+
+
+	int TauntRandom()
+	{
+		// Choose a random index of the taunts array.
+		int i = UnityEngine.Random.Range(0, taunts.Length);
+
+		// If it's the same as the previous taunt...
+		if(i == tauntIndex)
+			// ... try another random taunt.
+			return TauntRandom();
+		else
+			// Otherwise return this index.
+			return i;
+	}
+
+    void OnDisable()
+    {
+        if (controlsInputThread != null) controlsInputThread.Abort();
+        gameClient.Close();
+    }
+}//End of MonoBehaviour
